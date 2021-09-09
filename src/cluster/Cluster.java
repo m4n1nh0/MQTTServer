85f5876649fec4a78cccc75e184397e4b10aa9ee
Mariano Florencio Menonça <marianofmendonca@gmail.com>
2021-09-09 02:33:25 -0300

    Reelaboração do cluster
    Correção do programa anterior utilizando Thread com reanalise de cluster a cada 3 min

--- src/cluster/Cluster.java
+++ src/cluster/Cluster.java
@@ -1,11 +1,24 @@
 package cluster;
 
-public class Cluster {
+public class Cluster extends Thread {
 
-
-
-
-
-
-
+	//Controle do tempo de analise
+	private long TRound = 180000; //180 segundos, ou 3 min
+	// Quantidade máxima de seleção de CH que vao ser comparadas
+	static private int QTD_SENSORES_REDE = 100; //Define quantos sensores eu vou ter na minha rede
+    static private int MAX_VAL_X = 100;//metros //dita o maior valor da coordenada X do campo
+    static private int MAX_VAL_Y = 100;//metros //dita o maior valor da coordenada Y do campo
+    static private int QTD_MAX_CH = 20;//(int) (QNTD_SENSORES_NA_REDE * PERCENTUAL * 1.2); //Define a quantidade máxima de anuncios de CH que vao ser comparadas
+    static private int QTD_MAX_SENSORES = 50; //Quantidade máxima que um cluster head comporta
+    //areaTotal/Qntd de clusterheads
+    private double rangeCH = (MAX_VAL_X * MAX_VAL_Y) / 2;
+    // Controle
+    private boolean isCH = false; //define a situação do sensor na rede
+    private String id_CH = null; //Variável que guarda o id do sensor que é Cluster Head
+    private String[] id_SensorFolha = new String[QTD_MAX_SENSORES]; //Array que guarda os ids dos sensores
+    private int qtdRealFolhas = 0; //quantidade de sensores que respondem ao CH, menor que o total suportado de sensores pelo CH
+    // Iniciando o processo de seleção do CH baseado no artigo https://ieeexplore.ieee.org/document/8587930
+    public void run () {
+        // ...
+    }
 }
