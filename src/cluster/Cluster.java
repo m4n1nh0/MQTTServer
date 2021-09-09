693a4251f465daf34b1f52d926d6a15a718846c8
Mariano Florencio Menonça <marianofmendonca@gmail.com>
2021-09-09 02:36:21 -0300

    Update Cluster.java

--- src/cluster/Cluster.java
+++ src/cluster/Cluster.java
@@ -2,10 +2,10 @@ package cluster;
 
 public class Cluster extends Thread {
 
-	//Controle do tempo de analise
-	private long TRound = 180000; //180 segundos, ou 3 min
-	// Quantidade máxima de seleção de CH que vao ser comparadas
-	static private int QTD_SENSORES_REDE = 100; //Define quantos sensores eu vou ter na minha rede
+    //Controle do tempo de analise
+    private long TRound = 180000; //180 segundos, ou 3 min
+    // Quantidade máxima de seleção de CH que vao ser comparadas
+    static private int QTD_SENSORES_REDE = 100; //Define quantos sensores eu vou ter na minha rede
     static private int MAX_VAL_X = 100;//metros //dita o maior valor da coordenada X do campo
     static private int MAX_VAL_Y = 100;//metros //dita o maior valor da coordenada Y do campo
     static private int QTD_MAX_CH = 20;//(int) (QNTD_SENSORES_NA_REDE * PERCENTUAL * 1.2); //Define a quantidade máxima de anuncios de CH que vao ser comparadas
@@ -18,6 +18,8 @@ public class Cluster extends Thread {
     private String[] id_SensorFolha = new String[QTD_MAX_SENSORES]; //Array que guarda os ids dos sensores
     private int qtdRealFolhas = 0; //quantidade de sensores que respondem ao CH, menor que o total suportado de sensores pelo CH
     // Iniciando o processo de seleção do CH baseado no artigo https://ieeexplore.ieee.org/document/8587930
+    // Exemplo Thread http://www.inf.ufes.br/~pdcosta/ensino/2008-1-sistemas-operacionais/Slides/Aula17-1slide.pdf
+    // Exemplo Thread https://www.caelum.com.br/apostila-java-orientacao-objetos/apendice-programacao-concorrente-e-threads#e-as-classes-anonimas
     public void run () {
         // ...
     }
